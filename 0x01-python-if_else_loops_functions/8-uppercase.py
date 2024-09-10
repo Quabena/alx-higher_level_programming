da306ff44ec458b004b32f4e59791e3ff77ad670
Evans <ivanz.corbie@gmail.com>
2024-09-10 22:23:18 +0000

    8-uppercase.py fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,7 @@
 def uppercase(str):
     """Prints the given string in uppercase."""
     for char in str:
-        # Convert lowercase letters to uppercase
         if ord('a') <= ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
         print(char, end='')
-    print()  # Print newline at the end
+    print()
