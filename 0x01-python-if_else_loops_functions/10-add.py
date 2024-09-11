4f780b3bc2b6587bb21352c6df7639eae711224f
Evans <ivanz.corbie@gmail.com>
2024-09-11 01:51:47 +0000

    10-add.py file created

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    result = a + b  # Calculate the sum of a and b
+
+    return result
