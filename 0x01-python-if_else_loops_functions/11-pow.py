7a3f6e51e0dc2a988248b631ec6909f663914c3d
Evans <ivanz.corbie@gmail.com>
2024-09-11 01:56:08 +0000

    10-add.py made executable and 11-pow.py created

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the value.
+
+    Args:
+        a (int): The base integer.
+        b (int): The exponent integer.
+
+    Returns:
+        int: The result of a raised to the power of b.
+    """
+    result = a ** b  # Compute a raised to the power of b
+
+    return result
