47f28a72bbdbeb83b564a5248b6667848fa07b4c
Evans <ivanz.corbie@gmail.com>
2024-09-11 02:40:15 +0000

    3-print_alphabt.py fixed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
-print("".join("{:c}".format(i) for i in range(97, 123) if i != 101 and i != 113), end="")
+def print_alphabet():
+    """
+    Prints the ASCII alphabet in lowercase, excluding 'q' and 'e'.
+
+    Prints the letters from 'a' to 'z' except for 'q' and 'e',
+    using a single print function.
+    The output is not followed by a new line.
+    """
+    alphabet = (chr(i) for i in range(97, 123) if i != 101 and i != 113)
+    print("{}".format("".join(alphabet)), end="")
