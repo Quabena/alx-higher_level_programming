baf606dec77b61545ec5c512ba557dd46f98c4e5
Evans <ivanz.corbie@gmail.com>
2024-09-12 16:19:42 +0000

    0-add.py file created

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    # Importing the add function from add_0
+    from add_0 import add
+
+    # Assign values to variables a and b
+    a = 1
+    b = 2
+
+    # Call the add function with a and b and print the result
+    print("{} + {} = {}".format(a, b, add(a, b)))
