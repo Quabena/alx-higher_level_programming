898400ad1a66c43f9e9b30c915b615f476b7eb18
Evans <ivanz.corbie@gmail.com>
2024-09-30 20:33:50 +0000

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     printed_count = 0
     try:
@@ -7,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 printed_count += 1
     except IndexError:
-        pass  # Ignore IndexError if x exceeds the list length
-    print()  # Print a new line after printing integers
+        pass
+    print()
     return printed_count
