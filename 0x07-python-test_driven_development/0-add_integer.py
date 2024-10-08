e6b06ac6038a0511b2ab5db76e852b0abb6de43b
Evans <ivanz.corbie@gmail.com>
2024-10-08 20:03:27 +0000

    102-python.c

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
-"""
-This module provides a function to add two integers.
-"""
+'''
+
+This module has one function that adds up 2 integers
+
+'''
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers or floats (casts them to integers first).
+    '''Return the sum of two integers or floats as integers
 
     Args:
-        a: The first number (must be an integer or float).
-        b: The second number, default is 98 (must be an integer or float).
+        a: first argument
+        b: second argument
 
     Returns:
-        The integer addition of a and b after casting them to integers.
+        Sum of the two arguments
 
     Raises:
-        TypeError: If a or b is neither an integer nor a float.
-
-    Examples:
-        >>> add_integer(1, 2)
-        3
-        >>> add_integer(100, -2)
-        98
-        >>> add_integer(2.5, 3)
-        5
-        >>> add_integer(10)
-        108
-    """
-    if not isinstance(a, (int, float)):
+        TypeError: If either of the arguments not an integer or a float
+    '''
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    # Cast both a and b to integers (if they are floats)
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    return (int(a) + int(b))
