18974a1880db5b7617a589564e1311e8afadee92
Evans <ivanz.corbie@gmail.com>
2024-10-14 19:49:11 +0000

    7-base_geometry.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-    This module returns a list of all available attributes
+    This module returns the list of available attributes
     and methods of an object
 """
 
 
 def lookup(obj):
-    """This function looks for all attributes and methods of an object"""
+    """This functions looks out for all attributes and methods of an object"""
     return dir(obj)
