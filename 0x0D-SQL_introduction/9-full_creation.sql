6a15ca2b72500610c7fd431feff3738b31175a34
Evans <ivanz.corbie@gmail.com>
2024-11-20 00:45:33 +0000

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- A script that creates second_table and insert multiple rows
+-- Syntax: CREATE TABLE IF NOT EXISTS and INSERT INTO
+
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
