a00e45130f98da66fb47a871862c777ac8ca8670
Evans <ivanz.corbie@gmail.com>
2024-11-20 01:53:13 +0000

    12-no_cheating.sql

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- A script that updates the score of Bob to 10 in second_table
+-- Syntax: UPDATE table_name SET column_name = value WHERE condition
+
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
