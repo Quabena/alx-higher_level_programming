55f701d169bd4bdbd52313d9608e3ec20f2efebe
Evans <ivanz.corbie@gmail.com>
2024-11-20 02:17:55 +0000

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,11 @@
--- TASK 17: Convert database, table, and field to UTF8 (utf8mb4, utf8mb4_unicode_ci)
+-- A script that converts database, table, and field to UTF8 (utf8mb4, utf8mb4_unicode_ci)
 
 -- Convert the database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- Use the database
+USE hbtn_0c_0;
+
 -- Convert the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the 'name' field to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
