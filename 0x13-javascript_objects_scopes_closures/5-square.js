f88bfc2b8995daa23a5a2d3c808b3baf5eed61cd
Evans <ivanz.corbie@gmail.com>
2024-12-20 14:50:59 +0000

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-
-// Import the Rectangle class from 4-rectangle.js
 const Rectangle = require('./4-rectangle');
 
-// Define the Square class
 class Square extends Rectangle {
   constructor (size) {
-    // Call the parent class constructor with width and height set to size
     super(size, size);
   }
 }
