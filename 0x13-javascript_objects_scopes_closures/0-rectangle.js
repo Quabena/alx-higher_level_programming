029e3308ea00c472c1c493031880ada92c2db8c6
Evans <ivanz.corbie@gmail.com>
2024-12-20 14:20:12 +0000

    102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/**
+ * A rectangle class
+ */
+class Rectangle {}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/**
+ * A square class that inherits from the previous square class
+ */
+
+const OldSquare = require('./5-square');
+
+class Square extends OldSquare {
+  charPrint (c) {
+    const myChar = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += myChar;
+        y++;
+      }
+
+      console.log(myVar);
+    }
+  }
+}
+
+module.exports = Square;
