8aaab7f4c6e51a1b4027d245a4477e804749aed7
Evans <ivanz.corbie@gmail.com>
2024-12-20 14:46:50 +0000

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
-/**
- * A square class that inherits from rectangle class
- */
 
+// Import the Rectangle class from 4-rectangle.js
 const Rectangle = require('./4-rectangle');
 
+// Define the Square class
 class Square extends Rectangle {
   constructor (size) {
+    // Call the parent class constructor with width and height set to size
     super(size, size);
   }
 }
 
 module.exports = Square;
+
