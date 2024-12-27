9dfb1afc69e4c757f7c6778ee7aad4c4e17ddd49
Evans <ivanz.corbie@gmail.com>
2024-12-27 19:05:50 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
 """
-Display all values in the states table where name matches the argument
+Displays all values in the states table where name matches the argument
 """
-
 import MySQLdb
 import sys
 
-if __name__ = "__main__":
-    # Get MySQL credentials, database name, and state name from the argument
+if __name__ == "__main__":
+    # Get MySQL credentials, database name, and state name from arguments
     username, password, db_name, state_name = sys.argv[1], sys.argv[2],
     sys.argv[3], sys.argv[4]
 
-    # Connecting to the database
-    db = MySQL.connect(host="localhost", port=3306,
-                       user=username, passwd=password, db=db_name)
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
 
-    # A cursor to execute SQL queries
+    # Create a cursor to execute SQL queries
     cursor = db.cursor()
 
     # Execute the SQL query to find states matching the argument
-    query = "SELECT * FROM states WHERE name = {} ORDER BY id ASC;"
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;"
     .format(state_name)
     cursor.execute(query)
 
     # Fetch all the results
     states = cursor.fetchall()
 
-    # Print each state
+    # Print each state in the desired format
     for state in states:
         print(state)
 
-    # Close the cursor
+    # Close the cursor and connection
     cursor.close()
-
-    # Close the connection
     db.close()
