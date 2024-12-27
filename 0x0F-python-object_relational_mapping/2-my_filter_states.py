1afd4f232bf3831bab7cdd011d74fd6ae8d628f3
Evans <ivanz.corbie@gmail.com>
2024-12-27 19:03:38 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,16 +8,19 @@ import sys
 
 if __name__ = "__main__":
     # Get MySQL credentials, database name, and state name from the argument
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password, db_name, state_name = sys.argv[1], sys.argv[2],
+    sys.argv[3], sys.argv[4]
 
     # Connecting to the database
-    db = MySQL.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    db = MySQL.connect(host="localhost", port=3306,
+                       user=username, passwd=password, db=db_name)
 
     # A cursor to execute SQL queries
     cursor = db.cursor()
 
     # Execute the SQL query to find states matching the argument
-    query = "SELECT * FROM states WHERE name = {} ORDER BY id ASC;".format(state_name)
+    query = "SELECT * FROM states WHERE name = {} ORDER BY id ASC;"
+    .format(state_name)
     cursor.execute(query)
 
     # Fetch all the results
