c5f03b4260aaaf0cf482d3e509d2ace1138b7b48
Evans <ivanz.corbie@gmail.com>
2025-01-16 19:02:29 +0000

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # The script takes in a URL as an argument, sends a GET request with a custom header, and displays the body of the response
-curl -s "$1" -H "X-School-User-Id: 98"
+curl -sH "X-School-User-Id: 98" "$1"
 
