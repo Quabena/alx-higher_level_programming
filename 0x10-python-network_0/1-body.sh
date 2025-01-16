9d2ad8ee9df452e06489bb529121d3c92e07230b
Evans <ivanz.corbie@gmail.com>
2025-01-16 18:46:06 +0000

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the URL passed as the first argument and displays the body of the response if the status code is 200
-curl -s -L "$1" -o response.txt
-if [ "$(head -n 1 response.txt | cut -d' ' -f1)" == "HTTP/1.1" ] && [ "$(head -n 1 response.txt | cut -d' ' -f2)" == "200" ]; then
-  tail -n +2 response.txt
-fi
-
+# A script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
