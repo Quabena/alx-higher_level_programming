af1df45e2f1f5d3c42c64ae727f4b4ab58a45a0d
Evans <ivanz.corbie@gmail.com>
2025-01-17 16:14:22 +0000

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status using 'request'
+and displays the body of the response
+"""
+
+import requests
+
+if __name__ == "__main__":
+    # Sending a GET request to the URL
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    # Displaying/Printing the response body
+    print("Body response:")
+    print(f"\t- type: {type(response.txt)}")
+    print(f"\t- content: {response.text}")
