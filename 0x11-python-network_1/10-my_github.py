1c53abba532b5f3be95aab0cac6badd4a91c943d
Evans <ivanz.corbie@gmail.com>
2025-01-17 17:16:23 +0000

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display
+your id.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Retrieving GitHub username and PAT from argument
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    # GitHub API URL for the authenticated user
+    url = "https://api.github.com/user"
+
+    try:
+        # Performing a GET request with Basic Authentication
+        response = requests.get(url, auth=(username, token))
+
+        # Checking if response indicates a successful authentication
+        if response.status_code == 200:
+            user_data = response.json()
+            print(user_data.get("id"))
+        else:
+            print("None")
+    exception Exception as e:
+        # Print 'None' for any errors
+        print("None")
