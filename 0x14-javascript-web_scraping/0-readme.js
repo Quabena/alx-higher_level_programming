f8f4e6457b18f52b89effcdcfd6a298225d2a70b
Evans <ivanz.corbie@gmail.com>
2025-02-11 23:55:41 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 
+// Import the 'fs' (File System) module to handle file operations
 const fs = require('fs');
 
+// Get the file path from the first command-line argument
 const filePath = process.argv[2];
 
+// Read the file asynchronously with utf-8 encoding
 fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+  // Print the error object if an error occurs
+    console.log(err);
+  } else {
+  // Print the file content if reading is successful
+    console.log(data);
+  }
 });
